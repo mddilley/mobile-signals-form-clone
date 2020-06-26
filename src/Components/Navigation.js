c91be524e68b8cd87341db15074f1977a589cab3
Mike Dilley <mike.dilley@gmail.com>
2020-06-26 10:19:00 -0500

    Add react-hook-form, MUI, styled-components, nav and start form

---
+++ src/Components/Navigation.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import styled from "styled-components";
+
+const Navigation = () => {
+  const StyledAppBar = styled(AppBar)`
+    && {
+      background-color: #0d3e79;
+    }
+  `;
+
+  const LoginButton = styled(Button)`
+    && {
+      position: absolute;
+      right: 0;
+    }
+  `;
+
+  return (
+    <StyledAppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">Mobile Signals Work Order</Typography>
+        <LoginButton color="inherit">Login</LoginButton>
+      </Toolbar>
+    </StyledAppBar>
+  );
+};
+
+export default Navigation;
---
+++ src/Components/NewWorkOrder.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import styled from "styled-components";
+
+const NewWorkOrder = () => {
+  return <>test</>;
+};
+
+export default NewWorkOrder;
